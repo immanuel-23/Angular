a264a5d11e326452a6625eb174c3e4801fa6ccca
Immanuel <immanuelgk@gmail.com>
2022-10-06 12:43:39 +0530

    style binding

--- first-project/src/app/app.component.ts
+++ first-project/src/app/app.component.ts
@@ -25,7 +25,34 @@ export class AppComponent {
   }
   show:boolean=true;
   players=["immanuel","Virat","Sachin","rahul"]
+  batsman=[
+    {name:"Immanuel",country:"India",runs:"300"},
+    {name:"Virat",country:"India",runs:"40000"},
+    {name:"David",country:"Australia",runs:"30500"},
+    {name:"Dhoni",country:"India",runs:"70000"}
 
+  ]
+
+  scoreDetails=[
+    {name:'Immanuel',matchs:10,lastFiveScore:[100,20,10,40,50]},
+    {name:'Raj',matchs:4,lastFiveScore:[90,200,10,400,56]},
+    {name:'David',matchs:7,lastFiveScore:[100,20,10,40,50]},
+    {name:'Virat',matchs:7,lastFiveScore:[100,20,10,40,50]}
+
+  ]
+
+  fontColor:string='red';
+  bgColor:string='yellow';
+
+  toogleColor(){
+    if(this.fontColor=="red"){
+      this.bgColor='red';
+      this.fontColor='yellow'
+    }else{
+      this.fontColor='red';
+    this.bgColor='yellow';
+    }
+  }
 
 }
 // this is a class file
